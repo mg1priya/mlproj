4e04521d352139ecf5a457e6f38f4fa60dce5619
priya <mgpriya03@gmail.com>
2024-03-26 20:14:23 +0530

    donw

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+app=Flask(__name__)
+
+# app=application
+print("worls")
+
+@app.route('/index',methods = ['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predictdata():
+    print(request.method)
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        print("ffg")
+        data=CustomData(
+           ID=int(request.form.get('ID')),
+        LIMIT_BAL=int(request.form.get('LIMIT_BAL')),
+        SEX=int(request.form.get('SEX')),
+        EDUCATION=int(request.form.get('EDUCATION')),
+        MARRIAGE=int(request.form.get('MARRIAGE')),
+        AGE=int(request.form.get('AGE')),
+        PAY_0=int(request.form.get('PAY_0')),
+        PAY_2=int(request.form.get('PAY_2')),
+        PAY_3=int(request.form.get('PAY_3')),
+        PAY_4=int(request.form.get('PAY_4')),
+        PAY_5=int(request.form.get('PAY_5')),
+        PAY_6=int(request.form.get('PAY_5')),
+        BILL_AMT1=int(request.form.get('BILL_AMT1')),
+        BILL_AMT2=int(request.form.get('BILL_AMT2')),
+        BILL_AMT3=int(request.form.get('BILL_AMT3')),
+        BILL_AMT4=int(request.form.get('BILL_AMT4')),
+        BILL_AMT5=int(request.form.get('BILL_AMT5')),
+        BILL_AMT6=int(request.form.get('BILL_AMT6')),
+        PAY_AMT1=int(request.form.get('PAY_AMT1')),
+        PAY_AMT2=int(request.form.get('PAY_AMT2')),
+        PAY_AMT3=int(request.form.get('PAY_AMT3')),
+        PAY_AMT4=int(request.form.get('PAY_AMT4')),
+        PAY_AMT5=int(request.form.get('PAY_AMT5')),
+        PAY_AMT6=int(request.form.get('PAY_AMT6'))
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()   
+
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,18 @@
 import os
 import sys
-
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
+#import pickle
+import joblib
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path, obj):
+
+"""def save_object(file_path, obj):
+
     try:
         dir_path = os.path.dirname(file_path)
 
@@ -20,8 +22,17 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
+        raise CustomException(e, sys)"""
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        joblib.dump(obj, file_path)
+
+    except Exception as e:
         raise CustomException(e, sys)
-    
+
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -52,11 +63,18 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
 def load_object(file_path):
+    try:
+        # Use joblib.load to load the object
+        return joblib.load(file_path)
+
+    except Exception as e:
+        raise CustomException(e, sys)    
+"""def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e, sys)"""
